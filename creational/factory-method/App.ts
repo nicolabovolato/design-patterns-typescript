1a4fafd1b00b51d7e79be6387f5036f6a93b2808
Nicola Bovolato <nicola.bovolato@gmail.com>
2020-06-09 17:43:40 +0200

    refactoring

--- creational/factory-method/App.ts
+++ creational/factory-method/App.ts
@@ -1,18 +1,22 @@
-import { CarFactory, MotorcycleFactory } from "./Factories";
+import { VehicleFactory, CarFactory, MotorcycleFactory } from "./Factories";
 
 export default class FactoryMethod{
 
+    vehicle1: VehicleFactory;
+    vehicle2: VehicleFactory;
+
     constructor(){
-        this.init();
+
+        this.vehicle1 = new CarFactory();
+        this.vehicle2 = new MotorcycleFactory();
+
+        this.run();
     }
 
-    private init(): void {
+    private run(): void {
 
-        let car = new CarFactory();
-        let motorcycle =  new MotorcycleFactory();
-
-        car.start();
-        motorcycle.start();
+        this.vehicle1.start();
+        this.vehicle2.start();
     }
 
 }
