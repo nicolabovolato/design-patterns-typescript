4f79fd93ea0509fd9ff39174f7e214d79c634adf
Nicola Bovolato <nicola.bovolato@gmail.com>
2020-06-11 10:45:30 +0200

    added Composite

---
+++ structural/composite/App.ts
@@ -0,0 +1,36 @@
+import { ArmyRank, Division, Soldier } from "./Army";
+
+export default class Composite{
+
+    army: ArmyRank;
+
+    constructor(){
+
+        this.army = this.configure();
+        this.run();
+    }
+
+    configure(): ArmyRank {
+
+        let army = new Division("Army");
+
+        army.add(new Soldier("General", "Patton"));
+        
+        army.add(new Division("Paratroopers"));
+        army.add(new Soldier("Private", "Brown"));
+        army.add(new Soldier("Sergeant", "Davis"));
+        army.add(new Soldier("Private", "Miller"));
+
+        army.add(new Division("Infantry"));
+        army.add(new Soldier("Private", "Jones"));
+        army.add(new Soldier("Sergeant", "Smith"));
+        army.add(new Soldier("Private", "Williams"));
+
+        return army;
+    }
+
+    run(): void {
+
+        this.army.executeOrder();
+    }
+}
---
+++ structural/composite/Army.ts
@@ -0,0 +1,43 @@
+export interface ArmyRank {
+
+    executeOrder(): void;
+}
+
+export class Soldier implements ArmyRank {
+
+    rank: string;
+    name: string;
+
+    constructor(_rank: string, _name: string){
+
+        this.rank = _rank;
+        this.name = _name;
+    }
+
+    executeOrder(): void {
+        
+        console.log(`${this.rank} ${this.name} executed order`);
+    }
+}
+
+export class Division implements ArmyRank {
+
+    name: string;
+    private children: ArmyRank[] = [];
+
+    constructor(_name: string){
+        this.name = _name;
+    }
+
+    add(child: ArmyRank): void {
+        this.children.push(child);
+    }
+
+    remove(child: ArmyRank): void {
+        this.children.filter(_child => child === _child);
+    }
+
+    executeOrder(): void {
+        this.children.forEach( (child: ArmyRank) => child.executeOrder());
+    }
+}
