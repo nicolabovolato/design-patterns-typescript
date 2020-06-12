c4cafe8e13194d1e0c169b95303e8068d4bb2eca
Nicola Bovolato <nicola.bovolato@gmail.com>
2020-06-12 11:36:35 +0200

    added Flyweight

---
+++ structural/flyweight/App.ts
@@ -0,0 +1,28 @@
+import { Sky, CloudFactory } from "./Flyweight";
+
+export default class Flyweight {
+
+    sky: Sky;
+
+    constructor(){
+
+        this.sky = new Sky();
+        this.configure();
+        this.run();
+    }
+
+    configure(): void {
+
+        this.sky.addCloud(0, 0, "white");
+        this.sky.addCloud(1, 1, "white");
+        this.sky.addCloud(1, 0, "grey");
+        this.sky.addCloud(0, 1, "grey");
+    }
+
+    run(): void {
+
+        this.sky.rain();
+
+        console.log(`Cloud type instances: ${CloudFactory.getInstances()}`)
+    }
+}
---
+++ structural/flyweight/Flyweight.ts
@@ -0,0 +1,68 @@
+export class CloudFactory {
+
+    private static cloudTypes: CloudType[] = [];
+
+    static getCloudType(color: string) : CloudType {
+        
+        let found = this.cloudTypes.find((cloud: CloudType) => cloud.color === color);
+
+        if(!found || found === null){
+
+            found = new CloudType(color)
+            this.cloudTypes.push(found);
+        }
+        
+        return found;
+    }
+
+    static getInstances(): number {
+
+        return this.cloudTypes.length;
+    }
+}
+
+export class CloudType {
+    
+    color: string;
+    
+    constructor(_color: string) {
+        
+        this.color = _color;
+    }
+}
+
+export class Cloud {
+
+    x: number;
+    y: number;
+    type: CloudType;
+
+    constructor(_x: number, _y: number, _type: CloudType) {
+
+        this.x = _x;
+        this.y = _y;
+        this.type = _type;
+    }
+
+    rain(): void {
+        console.log(`Rain falls from ${this.type.color} cloud`)
+    }
+}
+
+export class Sky {
+
+    clouds: Cloud[] = [];
+
+    addCloud(x: number, y: number, color: string): void {
+        
+        let type = CloudFactory.getCloudType(color);
+        let cloud = new Cloud(x, y, type);
+
+        this.clouds.push(cloud);
+    }
+
+    rain(): void {
+        this.clouds.forEach((cloud: Cloud) => cloud.rain());
+    }
+}
+
