c40bbd0b04259a2baa1a9f587822a495957d5f04
Nicola Bovolato <nicola.bovolato@gmail.com>
2020-06-16 16:24:17 +0200

    added Visitor

---
+++ behavioral/visitor/App.ts
@@ -0,0 +1,23 @@
+import { ShapeAnalyzer } from "./Visitors";
+import { Rectangle, Circle } from "./Shapes";
+
+export default class VisitorEx {
+
+    analyzer: ShapeAnalyzer;
+
+    constructor(){
+
+        this.analyzer = new ShapeAnalyzer();
+
+        this.run();
+    }
+
+    run(): void {
+
+        let rectangle = new Rectangle(5, 10);
+        let circle = new Circle(8);
+
+        rectangle.accept(this.analyzer);
+        circle.accept(this.analyzer);
+    }
+}
---
+++ behavioral/visitor/Shapes.ts
@@ -0,0 +1,34 @@
+import { ShapeVisitor } from "./Visitors";
+
+export interface Shape {
+
+    accept(visitor: ShapeVisitor): void;
+}
+
+export class Circle implements Shape {
+    
+    radius: number;
+
+    constructor(_radius: number) {
+        this.radius= _radius;
+    }
+
+    accept(visitor: ShapeVisitor): void {
+        visitor.visitCircle(this);
+    }
+}
+
+export class Rectangle implements Shape {
+    
+    height: number
+    length: number
+
+    constructor(_height: number, _length: number) {
+        this.height= _height;
+        this.length= _length;
+    }
+
+    accept(visitor: ShapeVisitor): void {
+        visitor.visitRectangle(this);
+    }
+}
